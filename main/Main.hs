c1c9679d190f6ed0dac0e63f45e521c9a2e6a5c8
Ryan Orendorff <ryan@orendorff.io>
2019-06-07 09:15:24 -0600

    Add some haskell code that didn't make the presentation

--- main/Main.hs
+++ main/Main.hs
@@ -27,39 +27,53 @@ module Main where
 
 import           GHC.TypeLits
 import           Numeric.LinearAlgebra.Static
+import qualified Numeric.LinearAlgebra as LA
 import           Data.Proxy
 import           Numeric.LinearAlgebra.MatrixFree
+import qualified Numeric.LinearProgramming.L1 as L1
+import qualified Numeric.LinearProgramming.L1.Static as L1S
+import Data.Maybe (fromJust)
+import ControlSystems.DynamicSystems.Conversions.Static
+import System.Random
+import qualified Data.Vector.Storable as V
+
 
 f :: forall n . KnownNat n => Proxy n -> Integer
 f _ = natVal (Proxy :: Proxy n) +
       natVal (Proxy :: Proxy (n+2))
 
-(.*) :: forall m n. (KnownNat m, KnownNat n) => Double -> L m n -> L m n
-(.*) t m = konst t * m
-
--- Convert continuous time linear dynamical system to discrete time using a
--- given sampling period.
-c2d :: forall m n. (KnownNat m, KnownNat n) => Double -> L n n -> L n m -> (L n n, L n m)
-c2d t a b = splitCols $ fst (splitRows exp_block)
+l1_example :: (R 1000, LA.Vector ℝ, R 1000, LA.Vector ℝ)
+l1_example = (signal_static, x, x_static, lsq_sol)
   where
-    exp_block = expm (t .* block)
-    block = a ||| b
-              ===
-               0
+    m = 200
+    n = 1000
+    s = 30 -- Sparsity, number of non-zero elements in the signal
+
+    -- Random state transition matrix
+    a = LA.reshape n $ LA.randomVector 0 LA.Gaussian (n*m)
+
+    -- Create a signal with `s` elements being either -1 or 1.
+    g = mkStdGen 0
+    indx = take s $ randomRs (0, n - 1) g :: [Int]
+    r = take s $ fmap ([-1, 1] !!) $ randomRs (0, 1) g
+    signal = V.replicate n 0 V.// zip indx r
+    b = a LA.#> signal
+
+    -- Make dependently typed versions of the signal.
+    a_static = fromJust $ create a :: L 200 1000
+    signal_static = fromJust $ create signal :: R 1000
+    b_static = a_static #> signal_static
+
+    λ = 1
+    x = L1.l1Solve λ a b
+    x_static = L1S.l1Solve λ a_static b_static
+
+    lsq_sol = a LA.<\> b
+
+(signal, x, x_static, lsq_sol) = l1_example
 
 main :: IO ()
 main = do
   putStrLn "Hello, World!"
   print $ f (Proxy @3)
   print $ new_id "Hello"
-
-  let a = matrix [2, 3, 1, 0] :: L 2 2
-  let b = eye :: Sq 2
-  let t = 0.1 :: Double
-  let (ad, bd) = c2d t a b
-
-  putStrLn "Ad"
-  disp 4 ad
-
-  putStrLn "Bd"
-  disp 4 bd
